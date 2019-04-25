a9bc92dadc5638751992b1c8dbc8837e0ae5dbc2
kapilsahu <kapilsahu232@gmail.com>
2019-04-25 17:19:00 +0530

    done

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
+
+var ws = require('ws').Server;
+const http = require('http')
 const exp = require('express')
-var server = require('ws').Server;
+
+const app = exp()
 
 
+app.get('/home', (req,res)=>{
+  res.json({home : 'home'})
+})
 
+
+const server = http.createServer(app)
 const PORT = process.env.PORT || 3000
 
-var s = new server({ port: PORT});
+
+server.listen(PORT,()=> {
+    console.log(`the server is running on ${PORT} and date is ${new Date()}`)
+})
+
+
+/**
+* WebSocket server
+*/
+var s = new ws({
+    // WebSocket server is tied to a HTTP server. WebSocket
+    // request is just an enhanced HTTP request. For more info 
+    // http://tools.ietf.org/html/rfc6455#page-6
+    server
+  });
+
+
+
+// const PORT = process.env.PORT || 3000
+
+// var s = new server({ port: PORT});
 
 var name;
 
